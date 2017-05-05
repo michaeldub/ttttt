47df21269c3cb980c25f249704e873d4ace75ed8
Michael <Michael@DESKTOP-PV8N89Q>
2017-05-05 21:00:52 +0300

    qweqew

---
+++ WebApplication2/Models/Clas1231312s1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Clas1231312s1
+    {
+        private String name;
+        private String anothername;
+    }
+}
